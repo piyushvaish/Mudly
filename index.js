31e171d129c801bad2d1197c31f49ab101c2219b
piyush vaish <piyushvaish16@gmail.com>
2019-07-26 01:34:13 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+app.use(express.json());
+
+const genres = [
+    {id:1,name:'House'},
+    {id:2,name:'Techno'},
+    {id:3,name:'EDM'},
+    {id:4,name:'Trance'},
+    {id:5,name:'Psy Trance'},
+    {id:6,name:'Dubstep'},
+    {id:7,name:'Trap'},
+    {id:8,name:'Punjabi'},
+    {id:9,name:'Bollywood'},
+    {id:10,name:'Acoustic'},
+];
+
+app.get('/genres',(req,res)=>{
+    res.send(genres);
+});
+
+app.get('/genres/:id',(req,res)=>{
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (genre){
+        res.send(genre.name);
+        return;
+    }
+    res.status(404).send('Genre Not Found !');
+    
+});
+
+app.post('/genres',(req,res)=>{
+    const genre = {
+        id : genres.length + 1,
+        name : req.body.name
+    };
+    genres.push(genre);
+    res.send(genres);
+});
+
+app.put('/genres/:id',(req,res)=>{
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (genre){
+        genre.name = req.body.name;
+        res.send(genres);
+        return;
+    }
+    res.status(404).send('Genre Not Found !');
+});
+
+app.delete('/genres/:id',(req,res)=>{
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (genre){
+        genres.splice(genre,1);
+        res.send(genres);
+        return;
+    }
+    res.status(404).send('Genre Not Found !');
+})
+
+const port = process.env.port || 3000;
+app.listen(port,()=>{
+    console.log(`Listening on Port${port}`);
+})
